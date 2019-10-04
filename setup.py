af8912231960f71d3b446cf16704f44db308f12e
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2019-10-04 02:38:54 +0200

    Provide manifest, so that table files get installed

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lueck/pygradrana",
     packages=["gradrana"],#setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
